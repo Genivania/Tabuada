1e021ec86b36a3f2a9ed37b13ab03e69451a0c98
Genivania Macedo OLiveira <genimacedooliveira@gmail.com>
2022-09-08 11:28:49 -0300

    parte gráfica em andamento

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,7 +1,9 @@
 package br.senai.sp.jandira;
 
+import java.awt.Color;
 import java.awt.Font;
 
+//import javax.swing.ImageIcon;
 import javax.swing.event.InternalFrameAdapter;
 
 import br.senai.sp.jandira.gui.FrameTabuada;
@@ -15,10 +17,17 @@ public class AppTabuada {
 		tela.titulo = " TABUADA ";
 		tela.altura = 800 ;
 		tela.largura = 600 ;
+		tela.corFundoDaTabuada = new Color(239, 228, 176);
+		tela.corDaLetraDaTabuada = new Color(0, 106, 0);
+		tela.fonteConteudo= new Font("Arial", 0, 20);
+		tela.corDoCalcular = new Color(0, 179, 0);
+		tela.fontDoCalcular = new Font("Arial", Font.BOLD, 20);
+		tela.corDaFonteCalcular = new Color(255, 255, 255);
+		
+
 		tela.criarTela();
 		
 		
-	
 	
 //	EstruturaTabuada tabuada = new EstruturaTabuada();
 //	tabuada.multiplicador = 20;
--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -3,21 +3,38 @@ package br.senai.sp.jandira.gui;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+
+import br.senai.sp.jandira.model.EstruturaTabuada;
+
+
+
 
 public class FrameTabuada {
 
 	public String titulo;
 	public int largura;
 	public int altura;
-	public Color corDeFundoDeTela;
-	public Font FontDosLabels;
-	public Color corDoBotao;
+	public Color corFundoDaTabuada;
+	public Color corDaLetraDaTabuada;
+	public Font fonteConteudo;
+	public Color corDoCalcular;
+	public Font fontDoCalcular;
+	public Color corDaFonteCalcular;
 	
 	
+	
+	
+
 	
 	public void criarTela() {
 		
@@ -35,32 +52,125 @@ public class FrameTabuada {
 		// Criar os componentes da tela e colocar no painel (container)
 		JLabel labelTabuada = new JLabel();
 		labelTabuada.setText(" Tabuada 1.0");
-		labelTabuada.setBounds(200, 15, 700, 30);
+		labelTabuada.setBounds(100, 15, 700, 30);
 		labelTabuada.setForeground(new Color(237, 28, 36));
 		labelTabuada.setFont(new Font("Arial", Font.BOLD, 30)); 
 		
 		
-//		JLabel labelExplicacao = new JLabel();
-//		labelExplicacao.setText("Com a tabuada 1.0 os seus problemas acabaram. Calcule a tabuada que desejar em segundos!");
-//		labelExplicacao.setBounds(30, 40, 700, 30);
-//		labelExplicacao.setForeground(new Color(128, 128, 128));
-//		labelExplicacao.setFont(new Font("Arial", Font.BOLD, 15)); 
+		JLabel labelExplicacao = new JLabel();
+		labelExplicacao.setText("Com a tabuada 1.0 os seus problemas acabaram. Calcule ");
+		labelExplicacao.setBounds(120, 40, 700, 30);
+		labelExplicacao.setForeground(new Color(128, 128, 128));
+		labelExplicacao.setFont(new Font("Arial", 0, 15)); 
+		
+		JLabel labelExplicacao1 = new JLabel();
+		labelExplicacao1.setText("a tabuada que desejar em segundos!");
+		labelExplicacao1.setBounds(120, 60, 700, 30);
+		labelExplicacao1.setForeground(new Color(128, 128, 128));
+		labelExplicacao1.setFont(new Font("Arial", 0, 15)); 
+		
 		
 		JLabel labelMultiplicando = new JLabel();
 		labelMultiplicando.setText("Multiplicando: ");
-		labelMultiplicando.setBounds(50, 110, 300, 30);
+		labelMultiplicando.setFont(fonteConteudo);
+		labelMultiplicando.setBounds(140, 110, 300, 30);
+		
 		JTextField textFildMultiplicando = new JTextField();
-		textFildMultiplicando.setBounds(25, 300, 15, 0);
+		textFildMultiplicando.setBounds(330, 110, 200, 40);
+		textFildMultiplicando.setFont(new Font("Arial", 0 , 20)); 
 		
 		
+		
+		JLabel labelMinimoMulti = new JLabel();
+		labelMinimoMulti.setText("Mínimo Multiplicador: ");
+		labelMinimoMulti.setBounds(55, 170, 300, 30);
+		labelMinimoMulti.setFont(fonteConteudo);
+
+		JTextField textFildMinimoMulti = new JTextField();
+		textFildMinimoMulti.setBounds(330, 170, 200, 40);
+		textFildMinimoMulti.setFont(new Font("Arial", 0 , 20)); 
+		
+		
+		
+		JLabel labelMaximoMulti = new JLabel();
+		labelMaximoMulti.setText("Máximo Multiplicador: ");
+		labelMaximoMulti.setBounds(53, 230, 300, 30);
+		labelMaximoMulti.setFont(fonteConteudo);
+		
+		JTextField textFildMaximoMulti = new JTextField();
+		textFildMaximoMulti.setBounds(330, 230, 200, 40);
+		textFildMaximoMulti.setFont(new Font("Arial", 0 , 20)); 
+		
+		
+		JButton buttonCalcular = new JButton();
+		buttonCalcular.setText("Calcular");
+		buttonCalcular.setBounds(35, 300, 290, 50);
+		buttonCalcular.setBackground(corDoCalcular);
+		buttonCalcular.setFont(fontDoCalcular);
+		buttonCalcular.setForeground(corDaFonteCalcular);
+		
+		
+		//lista
+		JList<String> lista = new JList<String>();
+		JScrollPane scroll = new JScrollPane(lista);
+
+		scroll.setBounds(30, 420, 365, 250);
+		lista.setBackground(corFundoDaTabuada);
+		lista.setForeground(corDaLetraDaTabuada);
+		
+		
+		buttonCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if (textFildMultiplicando.getText().isEmpty()){
+					JOptionPane.showMessageDialog(null, "Multiplicando é obrigatório");
+					textFildMultiplicando.requestFocus();
+				}
+				else {
+					EstruturaTabuada tabuada = new EstruturaTabuada();
+					tabuada.multiplicador = Integer.parseInt(textFildMultiplicando.getText());
+					tabuada.minimoMult = Integer.parseInt(textFildMinimoMulti.getText());
+					tabuada.maximoMult = Integer.parseInt(textFildMaximoMulti.getText());
+					
+					lista.setListData(tabuada.getTabuada());
+				}
+			
+			}
+		});
+		
+		
+		JButton buttonLimpar = new JButton();
+		buttonLimpar.setText("Limpar ");
+		buttonLimpar.setBounds(330, 300, 200, 50);
+		buttonLimpar.setFont(new Font("Arial", Font.BOLD, 20)); 
+		buttonLimpar.setBackground(new Color(251, 181, 51));
+		buttonLimpar.setForeground(new Color(255, 255, 255));
+		
+		
+		JLabel labelResultado = new JLabel();
+		labelResultado.setText(" Resultado:");
+		labelResultado.setBounds(28, 380, 700, 30);
+		labelResultado.setFont(new Font("Arial", Font.BOLD, 20)); 
+
 		
 		//adicionar componentes
 		painel.add(labelTabuada);
-		//painel.add(labelExplicacao);
+		painel.add(labelExplicacao);
+		painel.add(labelExplicacao1);
 		painel.add(labelMultiplicando);
+		painel.add(textFildMultiplicando);
+		painel.add(textFildMinimoMulti);
+		painel.add(labelMinimoMulti);
+		painel.add(textFildMaximoMulti);
+		painel.add(labelMaximoMulti);
+		painel.add(buttonCalcular);
+		painel.add(buttonLimpar);
+		painel.add(labelResultado);
+		painel.add(scroll);
 		
 
-		
 		tela.setVisible(true);
 		
 		
